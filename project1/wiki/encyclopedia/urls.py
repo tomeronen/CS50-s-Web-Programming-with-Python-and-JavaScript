1beabe69e91a46cbf709cdd65ae46a6231a105a1
tomer <tomer.ronen1@mail.huji.ac.il>
2020-12-08 17:09:17 +0200

    finished search and index.html

--- project1/wiki/encyclopedia/urls.py
+++ project1/wiki/encyclopedia/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from . import views
 
+
 urlpatterns = [
     path("", views.index, name="index"),
+    path("wiki/search", views.search_wiki, name="search_wiki"),
     path("wiki/<str:wiki_title>", views.show_wiki, name="show_wiki")
 ]
--- project1/wiki/encyclopedia/views.py
+++ project1/wiki/encyclopedia/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 import markdown2
 from . import util
 
+
 def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
@@ -9,8 +10,24 @@ def index(request):
 
 
 def show_wiki(request, wiki_title):
-    wiki_content = markdown2.markdown(util.get_entry(wiki_title))
+    wiki_markdown = util.get_entry(wiki_title)
+    wiki_exists = wiki_markdown is not None
+    if wiki_exists:
+        wiki_content = markdown2.markdown(util.get_entry(wiki_title))
+    else:
+        wiki_content = ""
+
     return render(request, "encyclopedia/wiki.html", {
         "wiki_content": wiki_content,
-        "wiki_title": wiki_title
+        "wiki_title": wiki_title,
+        "wiki_exists": wiki_exists
+    })
+
+
+def search_wiki(request):
+    query = request.GET.get('q', '')
+    wiki_names = util.list_entries()
+    res = [i for i in wiki_names if query in i]
+    return render(request, "encyclopedia/search.html", {
+        "wiki_names": res
     })
