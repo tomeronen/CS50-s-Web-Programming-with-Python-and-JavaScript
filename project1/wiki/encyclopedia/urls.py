751c08c97a63466cb0c6861d009d7469f4bd891b
tomer <tomer.ronen1@mail.huji.ac.il>
2020-12-08 15:57:37 +0200

    add /wiki/WIKI_TITLE functionalty

--- project1/wiki/encyclopedia/urls.py
+++ project1/wiki/encyclopedia/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path("wiki/<str:wiki_title>", views.show_wiki, name="show_wiki")
 ]
--- project1/wiki/encyclopedia/views.py
+++ project1/wiki/encyclopedia/views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import render
-
+import markdown2
 from . import util
-
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
 
+
+def show_wiki(request, wiki_title):
+    wiki_content = markdown2.markdown(util.get_entry(wiki_title))
+    return render(request, "encyclopedia/wiki.html", {
+        "wiki_content": wiki_content,
+        "wiki_title": wiki_title
+    })
